1955978094bcafbfb4d9d95e948b7f9a46afb9d2
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2018-06-30 22:05:34 +0900

    Initial Commit

---
+++ client.go
@@ -0,0 +1,33 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+// clientはチャットを行なっている1人のユーザーを表します。
+type client struct {
+	// socketはこのクライアントのためのユーザーを表します。
+	socket *websocket.Conn
+	// sendはメッセージが送られるチャネルです。
+	send chan []byte
+	// roomはこのクライアントが参加しているチャットルームです。
+	room *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	r := newRoom()
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャットルームを開始します
+	go r.run()
+	// Webサーバーを開始します
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
