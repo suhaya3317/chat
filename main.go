52c37605c621ce2dec2504e90aa1207a08d89a0b
suhaya3317 <hayato_17n1200071@nnn.ed.jp>
2018-06-30 23:03:24 +0900

    Specify port number

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,10 +20,12 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
 	r := newRoom()
 	// ルート
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -30,7 +33,8 @@ func main() {
 	// チャットルームを開始します
 	go r.run()
 	// Webサーバーを開始します
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを起動します。ポート： ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
